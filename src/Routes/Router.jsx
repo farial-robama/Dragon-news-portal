a465b5cf03581e5a0be7e2de9419c0d09430b614
farial-robama <farialrobama15@gmail.com>
2025-10-20 01:30:28 +0200

    add base layout, header, navbar, latest news, and category components with routing setup

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router";
+import HomeLayout from "../Layouts/HomeLayout";
+import Home from "../Pages/Home";
+import CategoryNews from "../Pages/CategoryNews";
+
+const router = createBrowserRouter(
+    [
+        {
+            path: "/",
+            element: <HomeLayout></HomeLayout>,
+            children: [
+                {
+                    path: "",
+                    element: <Home></Home>
+                },
+                {
+                    path: "/category/:id",
+                    element: <CategoryNews></CategoryNews>
+                }
+            ]
+        }
+    ]
+)
+export default router;
